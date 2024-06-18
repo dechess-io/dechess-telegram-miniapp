5d678abd7ec945073d359f7d26fa247030cd71b1
luongtrieuvy202 <luongtrieuvy2016@gmail.com>
2024-06-19 01:24:21 +0700

    fix: update backend url

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,16 +2,16 @@ import { io } from 'socket.io-client'
 
 const token = localStorage.getItem('token')
 
-// export const socket = io('https://api.dechess.io', {
-//   extraHeaders: {
-//     Authorization: token !== null ? token : '',
-//   },
-//   autoConnect: true,
-// })
-
-export const socket = io('http://localhost:3001', {
+export const socket = io('https://api.dechess.io', {
   extraHeaders: {
     Authorization: token !== null ? token : '',
   },
   autoConnect: true,
 })
+
+// export const socket = io('http://localhost:3001', {
+//   extraHeaders: {
+//     Authorization: token !== null ? token : '',
+//   },
+//   autoConnect: true,
+// })
