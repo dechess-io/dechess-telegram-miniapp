f3f34a951d52623d2a41e203a7c8ee5c54e8e8c5
Natrix <76524304+luongtrieuvy202@users.noreply.github.com>
2024-08-04 00:30:13 +0700

    fix bot timer logic

--- src/components/Chess/BotGame/BotGame.tsx
+++ src/components/Chess/BotGame/BotGame.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState, useRef, useReducer } from 'react'
 import { Chess, Square } from 'chess.js'
 import GameOverPopUp from '../Popup/GameOverPopUp'
-import { convertToFigurineSan, isThreefoldRepetition } from '../../../utils/utils'
+import {
+  convertToFigurineSan,
+  getRandomValueFromList,
+  isThreefoldRepetition,
+} from '../../../utils/utils'
 import LoadingGame from '../../Loading/Loading'
 import Header from '../../Header/Header'
 import { useTonWallet } from '@tonconnect/ui-react'
@@ -13,22 +17,18 @@ import { engine } from '../../../services/worker'
 import { useLocation, useParams } from 'react-router-dom'
 import { socket } from '../../../services/socket'
 import { useAppDispatch, useAppSelector } from '../../../redux/store'
-import { selectGame } from '../../../redux/game/reducer'
 import {
-  addGameHistory,
-  addMoves,
   resetGame,
   resetMoveSelection,
-  setGame,
-  setGameDraw,
-  setGameOver,
-  setOptionSquares,
-  setPlayerTurn,
   setRightClickedSquares,
-  showPromotionDialog,
   setPlayer1,
   setPlayer2,
-  setNewMove,
+  setOpponentMove,
+  switchPlayerTurn,
+  setTurn,
+  setGameOver,
+  setWinner,
+  setLoser,
 } from '../../../redux/game/action'
 import { useTimer } from 'react-timer-hook'
 import {
@@ -47,7 +47,6 @@ const BotGame: React.FC<{}> = () => {
   const queryParams = new URLSearchParams(location.search)
 
   const wallet = useTonWallet()
-  const [turn, setTurn] = useState(wallet?.account.address ? wallet.account.address : 'player1')
   const [player1] = useState(wallet?.account.address ? wallet.account.address : 'player1')
   const [player2] = useState('bot')
   const [showPopup, setShowPopup] = useState(false)
@@ -60,10 +59,15 @@ const BotGame: React.FC<{}> = () => {
     minutes: timer1Minutes,
     start: startTimer1,
     pause: pauseTimer1,
+    resume: resumeTimer1,
     restart: restartTimer1,
   } = useTimer({
     expiryTimestamp: new Date(Date.now() + Number(queryParams.get('time')) * 60 * 1000),
     autoStart: false,
+    onExpire: () => {
+      gameDispatch(setGameOver(true))
+      gameDispatch(setWinner(false))
+    },
   })
 
   const {
@@ -71,10 +75,15 @@ const BotGame: React.FC<{}> = () => {
     minutes: timer2Minutes,
     start: startTimer2,
     pause: pauseTimer2,
+    resume: resumeTimer2,
     restart: restartTimer2,
   } = useTimer({
     expiryTimestamp: new Date(Date.now() + Number(queryParams.get('time')) * 60 * 1000),
     autoStart: false,
+    onExpire: () => {
+      gameDispatch(setGameOver(true))
+      gameDispatch(setWinner(false))
+    },
   })
 
   // const [opponentDisconnect, setOpponentDisconnect] = useState(false)
@@ -83,24 +92,15 @@ const BotGame: React.FC<{}> = () => {
     gameDispatch(resetGame())
     gameDispatch(setPlayer1(player1))
     gameDispatch(setPlayer2(player2))
-    if (gameState.playerTurn === player1) {
+    startTimer1()
+    if (gameState.turn === 'w') {
       startTimer1()
-    } else {
+      pauseTimer2()
+    } else if (gameState.turn === 'b') {
       startTimer2()
+      pauseTimer1()
     }
   }, [])
-
-  const isPlayerTimeout = () => {
-    return (
-      (gameState.playerTurn === gameState.player1 && timer1Minutes * 60 + timer1Seconds === 0) ||
-      (gameState.playerTurn === gameState.player2 && timer2Minutes * 60 + timer2Seconds === 0)
-    )
-  }
-
-  function getRandomValueFromList(list: any) {
-    const randomIndex = Math.floor(Math.random() * list.length)
-    return list[randomIndex]
-  }
 
   engine.onmessage = function (event) {
     if (event.data.split(' ')[0] === 'bestmove') {
@@ -113,17 +113,20 @@ const BotGame: React.FC<{}> = () => {
             to: data[1].substring(2, 4),
           })
           gameDispatch(
-            setNewMove({
+            setOpponentMove({
               turn: gameCopy.turn(),
               fen: gameCopy.fen(),
               san: data[1],
               board: gameCopy,
-              history: [...gameState.history, gameCopy.fen()],
+              history: [],
             })
           )
+
           gameDispatch(resetMoveSelection())
+          resumeTimer1()
+          pauseTimer2()
         },
-        getRandomValueFromList([1000, 2000, 3000, 4000])
+        getRandomValueFromList([4000])
       )
     }
   }
@@ -135,21 +138,19 @@ const BotGame: React.FC<{}> = () => {
 
   const makeMove = (foundMove: any, square: Square) => {
     const newState = gameDispatch(handleMoveThunk({ foundMove, square }))
+    pauseTimer1()
+    resumeTimer2()
     sendPositionToEngine(newState.board.fen())
   }
 
   const onSquareClick = (square: Square) => {
     const { isMove, foundMove } = gameDispatch(onSquareClickThunk(square, wallet))
     if (isMove) {
+      gameDispatch(setTurn('b'))
+      gameDispatch(switchPlayerTurn())
       makeMove(foundMove, square)
     }
   }
-
-  // useEffect(() => {
-  //   if (isThreefoldRepetition(gameState.history)) {
-  //     gameDispatch(setGameDraw(true))
-  //   }
-  // }, [history])
 
   useEffect(() => {
     if (gameState.isGameOver || gameState.isGameDraw) {
@@ -178,29 +179,9 @@ const BotGame: React.FC<{}> = () => {
   }
 
   useEffect(() => {
-    if (
-      !gameState.isGameDraw &&
-      !gameState.isGameOver &&
-      !gameState.isWinner &&
-      !gameState.isLoser
-    ) {
-      if (gameState.playerTurn === gameState.player1 && timer1Minutes * 60 + timer1Seconds > 0) {
-        startTimer1()
-        pauseTimer2()
-      } else if (
-        gameState.playerTurn === gameState.player2 &&
-        timer2Minutes * 60 + timer2Seconds > 0
-      ) {
-        startTimer2()
-        pauseTimer1()
-      } else if (isPlayerTimeout() && !gameState.isGameOver) {
-        // gameDispatch(setGameOver(true))
-        // emitGameOver(socket, gameState, location.pathname.split('/')[2])
-      }
-    }
-
-    return () => {}
-  }, [gameState.turn, timer1Minutes, timer1Seconds, timer2Minutes, timer2Seconds])
+    pauseTimer1()
+    pauseTimer2()
+  }, [gameState.isGameOver, gameState.isWinner, gameState.isLoser])
 
   const theme = isAndroid ? 'material' : 'ios'
 
