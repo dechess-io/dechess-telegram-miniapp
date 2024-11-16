95c77dbac1a82be88eaa1ec921122ad592e8358e
thienba <macm3pro@Thien-Ba.local>
2024-11-16 18:29:35 +0700

    feat(mint-nft): complete ui

--- src/pages/MintNFTPage.tsx
+++ src/pages/MintNFTPage.tsx
@@ -3,6 +3,7 @@ import { sliceAddress } from '../utils/utils'
 import iconWallet from '../../public/icon-wallet.svg'
 import iconChevronDown from '../../public/icon-chevron-down.svg'
 import { useMemo } from 'react'
+import ButtonV2 from '../components/Button/ButtonV2'
 
 const MintNFTPage: React.FC = () => {
   const address = localStorage.getItem('address')
@@ -25,6 +26,16 @@ const MintNFTPage: React.FC = () => {
           <img width={30} height={30} src={iconChevronDown} alt="Chevron Down" />
         </div>
       </div>
+
+      <div className="container mt-4 h-[calc(100vh_-_52px_-_113px)] flex justify-center items-center">
+        <div className="flex flex-col gap-2 items-center">
+          <img className="w-32 h-32 animate-pulse" src="/icon-earn.svg" alt="NFT Mint" />
+          <p className="text-white text-sm">ID: 1234567890</p>
+          <p className="text-white text-sm">Name: Dechess Master</p>
+          <ButtonV2>Mint NFT</ButtonV2>
+        </div>
+      </div>
+
       <FooterV2 />
     </div>
   )
